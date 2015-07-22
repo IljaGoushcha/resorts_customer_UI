44341a98b2f71b6e62523f142c4e044e9585e032
IljaGoushcha <iljagou@yahoo.com>
2015-07-22 15:06:16 -0400

    prove of concept, new tree structure tested

--- app/navigation/controllers/about.ctrl.js
+++ app/navigation/controllers/about.ctrl.js
@@ -7,37 +7,121 @@ angular.module('Navigation')
   $scope.my_data_json =
   {
     "tenantId": 1000,
-    "eventTypes": {
-      "eventType01": {
+    "eventTypes":
+    [
+      {
+        "name": "event type 1",
         "isOpen": false,
         "displayName": "email received",
-        "attributes": {
-          "email": {},
-          "body": {},
-          "date": {}
-        }
+        "attributes": [
+          {
+            "name": "email provider",
+            "isOpen": false,
+            "values": [
+              {"name": "gmail"},
+              {"name": "yahoo"},
+              {"name": "aol"}
+            ]
+          },
+          {
+            "name": "DNS server",
+            "isOpen": false,
+            "values": [
+              {"name": "dns server 1"},
+              {"name": "dns server 2"},
+              {"name": "dns server 3"}
+            ]
+          },
+          {
+            "name": "date",
+            "isOpen": false,
+            "values": [
+              {"name": "March 13, 2015"},
+              {"name": "March 28, 2014"},
+              {"name": "April 7, 1969"}
+            ]
+          }
+        ]
       },
-      "eventType02": {
+      {
+        "name": "event type 2",
         "isOpen": false,
         "displayName": "purchase",
-        "attributes": {
-          "item": {},
-          "date": {},
-          "customer": {}
-        }
-
+        "attributes": [
+          {
+            "name": "item",
+            "isOpen": false,
+            "values": [
+              {"name": "shoes"},
+              {"name": "book"},
+              {"name": "iPad"}
+            ]
+          },
+          {
+            "name": "date",
+            "isOpen": false,
+            "values": [
+              {"name": "Winter 2001"},
+              {"name": "Spring 2013"},
+              {"name": "Summer 2005"}
+            ]
+          },
+          {
+            "name": "customer",
+            "isOpen": false,
+            "values": [
+              {"name": "Ilja"},
+              {"name": "Bob"},
+              {"name": "David"}
+            ]
+          }
+        ]
       },
-      "eventType03": {
+      {
+        "name": "event type 3",
         "isOpen": false,
         "displayName": "reward received",
-        "attributes": {
-          "rewardType": {},
-          "customer": {},
-          "date": {}
-
-        }
+        "attributes": [
+          {
+            "name": "rewardType",
+            "isOpen": false,
+            "values": [
+              {"name": "coupon"},
+              {"name": "gift card"},
+              {"name": "free hotel room"}
+            ]
+          },
+          {
+            "name": "customer",
+            "isOpen": false,
+            "values": [
+              {"name": "Ilja"},
+              {"name": "Bob"},
+              {"name": "David"}
+            ]
+          },
+          {
+            "name": "date",
+            "isOpen": false,
+            "values": [
+              {"name": "Winter 2001"},
+              {"name": "Spring 2013"},
+              {"name": "Summer 2005"}
+            ]
+          }
+        ]
       }
-    }
+    ]
+  };
+
+  $scope.toggleAttributes = function(index) {
+    $scope.my_data_json.eventTypes[index].isOpen = !$scope.my_data_json.eventTypes[index].isOpen;
+    console.log(index);
+  };
+
+  $scope.toggleValues = function(parentIndex, index) {
+    $scope.my_data_json.eventTypes[parentIndex].attributes[index].isOpen = !$scope.my_data_json.eventTypes[parentIndex].attributes[index].isOpen;
+    console.log(parentIndex + " " + index);
   };
 
 });
--- app/navigation/directives/tree-node.dir.js
+++ app/navigation/directives/tree-node.dir.js
@@ -1,13 +1,34 @@
 angular.module('Navigation')
 .directive('treeNode', function() {
 
+  'use strict'
+
   return {
     restrict: 'E',
     templateUrl: '../../../partials/navigation/tree-node.html',
     scope: {
-      eventType: "=eventType"
+      eventType: "=eventType",
+      bindedEventType: "=ngModel"
     },
-    link: function() {
+    link: function(scope, element, attrs) {
+
+      console.log(element.find("span"));
+
+      var testFunction = function() {
+        console.log("inside test function");
+        scope.bindedEventType.isOpen = true;
+
+      };
+
+      element.find("span").on('click', testFunction); //if multiple spans, use element.find('span')[1]
+
+      scope.expanderImgPath = '';
+      if (scope.eventType.isOpen === true) {
+        scope.expanderImgPath = 'hi';
+      } else if (scope.eventType.isOpen === false) {
+        scope.expanderImgPath = 'hello';
+      }
+      console.log("inside link: " + scope.expanderImgPath);
 
     }
   };
